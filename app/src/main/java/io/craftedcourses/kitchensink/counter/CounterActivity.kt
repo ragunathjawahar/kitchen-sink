8677d7df4d1cdf5ea6d7c9509eeb2a9dfe0c1630
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-02-23 17:48:20 +0530

    Counter, wire model to Activity.

--- app/src/main/java/io/craftedcourses/kitchensink/counter/CounterActivity.kt
+++ app/src/main/java/io/craftedcourses/kitchensink/counter/CounterActivity.kt
@@ -2,11 +2,51 @@ package io.craftedcourses.kitchensink.counter
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.jakewharton.rxbinding2.view.clicks
 import io.craftedcourses.kitchensink.R
+import io.craftedcourses.kitchensink.mvi.Binding
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.BehaviorSubject
+import kotlinx.android.synthetic.main.activity_counter.*
 
-class CounterActivity : AppCompatActivity() {
+class CounterActivity : AppCompatActivity(), CounterView {
+  private var newBinding = true
+
+  private lateinit var disposable: Disposable
+  private val bindings: BehaviorSubject<Binding>    = BehaviorSubject.createDefault(Binding.NEW)
+  private val states: BehaviorSubject<CounterState> = BehaviorSubject.create()
+
+  private val intentions: CounterIntentions by lazy {
+    CounterIntentions(incrementButton.clicks(), decrementButton.clicks())
+  }
+
+  private val viewDriver: CounterViewDriver by lazy {
+    CounterViewDriver(this)
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_counter)
+  }
+
+  override fun onStart() {
+    super.onStart()
+    val counterStates = CounterModel.bind(intentions, bindings, states).share()
+    counterStates.subscribe(states)
+    disposable = counterStates.subscribe { viewDriver.render(it) }
+
+    bindings.onNext(if (newBinding) Binding.NEW else Binding.RESTORED)
+  }
+
+  override fun onStop() {
+    if (!disposable.isDisposed) {
+      disposable.dispose()
+      newBinding = false
+    }
+    super.onStop()
+  }
+
+  override fun displayCounter(value: Int) {
+    counterTextView.text = value.toString()
   }
 }
--- app/src/main/java/io/craftedcourses/kitchensink/mvi/Binding.kt
+++ app/src/main/java/io/craftedcourses/kitchensink/mvi/Binding.kt
@@ -1,5 +1,5 @@
 package io.craftedcourses.kitchensink.mvi
 
 enum class Binding {
-  NEW
+  NEW, RESTORED
 }
