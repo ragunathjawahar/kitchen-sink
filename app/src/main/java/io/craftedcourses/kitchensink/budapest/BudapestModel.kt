4de0450bf57ab7c23839e8b12d5c067e4639e8c5
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-02-24 09:42:09 +0530

    BudapestModel, handle enter name use case.

--- app/src/main/java/io/craftedcourses/kitchensink/budapest/BudapestModel.kt
+++ app/src/main/java/io/craftedcourses/kitchensink/budapest/BudapestModel.kt
@@ -8,8 +8,24 @@ object BudapestModel {
       intentions: BudapestIntentions,
       bindings: Observable<Binding>
   ): Observable<BudapestState> {
+    return Observable.merge(
+        newBindingUseCase(bindings),
+        enterNameUseCase(intentions.enterName())
+    )
+  }
+
+  private fun newBindingUseCase(
+      bindings: Observable<Binding>
+  ): Observable<BudapestState> {
     return bindings
         .filter { it == Binding.NEW }
         .map    { BudapestState.STRANGER }
+  }
+
+  private fun enterNameUseCase(
+      enterName: Observable<String>
+  ): Observable<BudapestState> {
+    return enterName
+        .map { BudapestState(it) }
   }
 }
--- app/src/test/java/io/craftedcourses/kitchensink/budapest/BudapestModelTest.kt
+++ app/src/test/java/io/craftedcourses/kitchensink/budapest/BudapestModelTest.kt
@@ -5,20 +5,43 @@ import io.craftedcourses.kitchensink.infra.emits
 import io.craftedcourses.kitchensink.mvi.Binding
 import io.reactivex.observers.TestObserver
 import io.reactivex.subjects.PublishSubject
+import org.junit.Before
 import org.junit.Test
 
 @Suppress("IllegalIdentifier") // JVM tests can have space in function names
 class BudapestModelTest {
-  @Test fun `user sees stranger state when UI is setup`() {
-    val nameTextChanges = PublishSubject.create<CharSequence>()
-    val intentions = BudapestIntentions(nameTextChanges)
-    val bindings = PublishSubject.create<Binding>()
+  private lateinit var nameTextChanges: PublishSubject<CharSequence>
+  private lateinit var intentions: BudapestIntentions
+  private lateinit var bindings: PublishSubject<Binding>
+  private lateinit var observer: TestObserver<BudapestState>
 
-    val observer = TestObserver<BudapestState>()
+  @Before fun setup() {
+    nameTextChanges = PublishSubject.create<CharSequence>()
+    intentions = BudapestIntentions(nameTextChanges)
+    bindings = PublishSubject.create<Binding>()
+    observer = TestObserver()
+
     BudapestModel.bind(intentions, bindings).subscribe(observer)
+  }
 
+  @Test fun `user sees stranger state when UI is setup`() {
     observer.assertStates(
-        { bindings.onNext(Binding.NEW) } emits BudapestState.STRANGER
+        { newBinding() } emits BudapestState.STRANGER
     )
+  }
+
+  @Test fun `user sees a name that they entered`() {
+    observer.assertStates(
+        { newBinding()      } emits BudapestState.STRANGER,
+        { enterName("Alan") } emits BudapestState("Alan")
+    )
+  }
+
+  private fun enterName(name: String) {
+    nameTextChanges.onNext(name)
+  }
+
+  private fun newBinding() {
+    bindings.onNext(Binding.NEW)
   }
 }
