060d5f2e3e586fa1166d97d0f174d6b8ff13bd55
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-02-24 12:52:57 +0530

    MviActivity, create base class.

--- app/src/main/java/io/craftedcourses/kitchensink/counter/CounterActivity.kt
+++ app/src/main/java/io/craftedcourses/kitchensink/counter/CounterActivity.kt
@@ -1,17 +1,12 @@
 package io.craftedcourses.kitchensink.counter
 
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.jakewharton.rxbinding2.view.clicks
 import io.craftedcourses.kitchensink.R
-import io.craftedcourses.kitchensink.mvi.MviDelegate
+import io.craftedcourses.kitchensink.mvi.MviActivity
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.counter_activity.*
 
-class CounterActivity : AppCompatActivity(), CounterView {
-  private val mviDelegate: MviDelegate<CounterState> by lazy {
-    MviDelegate<CounterState>()
-  }
-
+class CounterActivity : MviActivity<CounterState>(), CounterView {
   private val intentions: CounterIntentions by lazy {
     CounterIntentions(incrementButton.clicks(), decrementButton.clicks())
   }
@@ -20,38 +15,16 @@ class CounterActivity : AppCompatActivity(), CounterView {
     CounterViewDriver(this)
   }
 
+  override fun layoutResId(): Int =
+      R.layout.counter_activity
+
+  override fun bindingFunction(): () -> Observable<CounterState> =
+      { CounterModel.bind(intentions, bindings, states) }
+
+  override fun renderFunction(): (CounterState) -> Unit =
+      { state -> viewDriver.render(state) }
+
   override fun displayCounter(value: Int) {
     counterTextView.text = value.toString()
-  }
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.counter_activity)
-    mviDelegate.onCreate(savedInstanceState)
-  }
-
-  override fun onStart() {
-    super.onStart()
-    val bindingFunction = {
-      CounterModel.bind(intentions, mviDelegate.bindings(), mviDelegate.states())
-    }
-    val renderFunction: (CounterState) -> Unit = { viewDriver.render(it) }
-
-    mviDelegate.onStart(bindingFunction, renderFunction)
-  }
-
-  override fun onStop() {
-    mviDelegate.onStop()
-    super.onStop()
-  }
-
-  override fun onSaveInstanceState(outState: Bundle?) {
-    mviDelegate.onSaveInstanceState(outState)
-    super.onSaveInstanceState(outState)
-  }
-
-  override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-    super.onRestoreInstanceState(savedInstanceState)
-    mviDelegate.onRestoreInstanceState(savedInstanceState)
   }
 }
