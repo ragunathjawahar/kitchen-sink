9651c92a63f7bf80545fef42048b0dee2e6da598
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-02-24 11:16:00 +0530

    BudapestModel, handle restored subscription use case.

--- app/src/main/java/io/craftedcourses/kitchensink/budapest/BudapestModel.kt
+++ app/src/main/java/io/craftedcourses/kitchensink/budapest/BudapestModel.kt
@@ -2,14 +2,17 @@ package io.craftedcourses.kitchensink.budapest
 
 import io.craftedcourses.kitchensink.mvi.Binding
 import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
 
 object BudapestModel {
   fun bind(
       intentions: BudapestIntentions,
-      bindings: Observable<Binding>
+      bindings: Observable<Binding>,
+      states: Observable<BudapestState>
   ): Observable<BudapestState> {
     return Observable.merge(
         newBindingUseCase(bindings),
+        restoredBindingUseCase(bindings, states),
         enterNameUseCase(intentions.enterName())
     )
   }
@@ -20,6 +23,18 @@ object BudapestModel {
     return bindings
         .filter { it == Binding.NEW }
         .map    { BudapestState.STRANGER }
+  }
+
+  private fun restoredBindingUseCase(
+      bindings: Observable<Binding>,
+      states: Observable<BudapestState>
+  ): Observable<BudapestState>? {
+    val combiner = BiFunction<Binding, BudapestState, BudapestState> { _, previousState ->
+      previousState
+    }
+    return bindings
+        .filter { it == Binding.RESTORED }
+        .withLatestFrom(states, combiner)
   }
 
   private fun enterNameUseCase(
--- app/src/test/java/io/craftedcourses/kitchensink/budapest/BudapestModelTest.kt
+++ app/src/test/java/io/craftedcourses/kitchensink/budapest/BudapestModelTest.kt
@@ -3,7 +3,10 @@ package io.craftedcourses.kitchensink.budapest
 import io.craftedcourses.kitchensink.infra.assertStates
 import io.craftedcourses.kitchensink.infra.emits
 import io.craftedcourses.kitchensink.mvi.Binding
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
 import io.reactivex.observers.TestObserver
+import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
 import org.junit.Before
 import org.junit.Test
@@ -13,15 +16,21 @@ class BudapestModelTest {
   private lateinit var nameTextChanges: PublishSubject<CharSequence>
   private lateinit var intentions: BudapestIntentions
   private lateinit var bindings: PublishSubject<Binding>
+  private lateinit var states: BehaviorSubject<BudapestState>
+  private lateinit var statesDisposable: Disposable
+  private lateinit var budapestModelObservable: Observable<BudapestState>
   private lateinit var observer: TestObserver<BudapestState>
 
   @Before fun setup() {
     nameTextChanges = PublishSubject.create<CharSequence>()
-    intentions = BudapestIntentions(nameTextChanges)
-    bindings = PublishSubject.create<Binding>()
-    observer = TestObserver()
+    intentions      = BudapestIntentions(nameTextChanges)
+    bindings        = PublishSubject.create()
+    states          = BehaviorSubject.create()
+    observer        = TestObserver()
 
-    BudapestModel.bind(intentions, bindings).subscribe(observer)
+    budapestModelObservable = BudapestModel.bind(intentions, bindings, states).share()
+    statesDisposable = budapestModelObservable.subscribe { states.onNext(it) }
+    budapestModelObservable.subscribe(observer)
   }
 
   @Test fun `user sees stranger state when UI is setup`() {
@@ -37,11 +46,44 @@ class BudapestModelTest {
     )
   }
 
-  private fun enterName(name: String) {
-    nameTextChanges.onNext(name)
+  @Test fun `user sees previous state when subscription is restored`() {
+    // Setup
+    observer.assertStates(
+        { newBinding()      } emits BudapestState.STRANGER,
+        { enterName("Alan") } emits BudapestState("Alan")
+    )
+    val lastKnownState = states.value
+
+    // Act
+    val newObserver = disposeAndResubscribe()
+
+    // Assert
+    newObserver.assertStates(
+        { restoredBinding() } emits lastKnownState
+    )
   }
 
   private fun newBinding() {
     bindings.onNext(Binding.NEW)
+  }
+
+  private fun restoredBinding() {
+    bindings.onNext(Binding.RESTORED)
+  }
+
+  private fun enterName(name: String) {
+    nameTextChanges.onNext(name)
+  }
+
+  private fun disposeAndResubscribe(): TestObserver<BudapestState> {
+    // Dispose
+    statesDisposable.dispose()
+    observer.dispose()
+
+    // Subscribe again...
+    val observer = TestObserver<BudapestState>()
+    statesDisposable = budapestModelObservable.subscribe { states.onNext(it) }
+    budapestModelObservable.subscribe(observer)
+    return observer
   }
 }
